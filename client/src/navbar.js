c5322ea2230af3358c1c5f90f81034369cc82ab3
ElenaSj <elena.sjoblom@gmail.com>
2023-07-19 12:50:19 +0300

    add different navbar for desktop

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -9,10 +9,14 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { useMediaQuery } from 'react-responsive'
+import Paper from '@mui/material/Paper';
 
 function NavBar() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [language, changeLanguage] = React.useState('Suomi')
+  const isDesktop = useMediaQuery({query: '(min-width: 1224px)'})
+  const isMobile = useMediaQuery({query: '(max-width: 1224px'})
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [language, changeLanguage] = React.useState('Suomi')
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -24,7 +28,7 @@ function NavBar() {
     setAnchorEl(null);
   };
 
-    return (
+    return (<>{isMobile &&
         <Box sx={{ flexGrow: 1 }}>
           <AppBar position="static">
             <Toolbar>
@@ -64,7 +68,63 @@ function NavBar() {
       </Menu>
             </Toolbar>
           </AppBar>
-        </Box>
+        </Box>}
+        {isDesktop && <div style={{borderBottom: '1px solid grey'}}>
+        <Box sx={{  display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: '100 %',
+          height: 30,
+        }, }}>
+              <Typography variant="h6" component="div" sx={{ my: 2, flexGrow: 1 }}>
+                Hae varhaiskasvatukseen
+              </Typography>
+              
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              
+              <Button
+            id="basic-button"
+              >
+                Tee hakemus
+              </Button>
+              <Button
+            id="basic-button"
+              >
+                Keskeneräiset hakemukset
+              </Button>
+              <Button
+              id="basic-button"
+              >
+                Kirjaudu ulos
+              </Button>
+              
+          </Box>
+              <Button
+               id="basic-button"
+               aria-controls={open ? 'basic-menu' : undefined}
+               aria-haspopup="true"
+               aria-expanded={open ? 'true' : undefined}
+               onClick={handleClick} 
+               endIcon={<ExpandMoreIcon />} color="inherit"
+               >
+                {language}</Button>
+                <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={()=>handleClose('edellinen')}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={()=>handleClose('Suomi')}>Suomi</MenuItem>
+        <MenuItem onClick={()=>handleClose('Svenska')}>Svenska</MenuItem>
+        <MenuItem onClick={()=>handleClose('English')}>English</MenuItem>
+      </Menu>
+    
+        </Box></div>}
+        </>
       );
 }
 
